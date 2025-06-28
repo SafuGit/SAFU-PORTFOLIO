5fe45147b39875e113fa08cf38e6ded62e3b7bc4
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-06-28 10:48:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className='App font-poppins'>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BiHome, BiLike } from "react-icons/bi";
+import { CgCode, CgProfile } from "react-icons/cg";
+import { PiPhone } from "react-icons/pi";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="flex NavbarStyle rounded-full justify-center gap-4 navbar w-fit p-2! text-white border border-y-gray-950">
+        <div className="rounded-full p-2! flex gap-1 hover:bg-green-950 transition-colors duration-300 ease-in-out">
+          <BiHome className="text-xl"></BiHome>
+          <a href="">Home</a>
+        </div>
+        <div className="rounded-full p-2! flex gap-1 hover:bg-green-950 transition-colors duration-300 ease-in-out">
+          <CgProfile className="text-xl"></CgProfile>
+          <a href="">About Me</a>
+        </div>
+        <div className="rounded-full p-2! flex gap-1 hover:bg-green-950 transition-colors duration-300 ease-in-out">
+          <CgCode className="text-2xl"></CgCode>
+          <a href="">Skills</a>
+        </div>
+        <div className="rounded-full p-2! flex gap-1 hover:bg-green-950 transition-colors duration-300 ease-in-out">
+          <BiLike className="text-xl"></BiLike>
+          <a href="">Hobbies</a>
+        </div>
+        <div className="rounded-full p-2! flex gap-1 items-center hover:bg-green-950 transition-colors duration-300 ease-in-out">
+          <PiPhone className="text-xl"></PiPhone>
+          <a href="">Contact</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
